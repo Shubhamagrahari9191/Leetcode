72afe84c69a02a34eb9e5978ac64f86060fe26c4
SHUBHAM <shubhamagrahari9191@gmail.com>
2026-09-13 00:37:49 +0530

    Time: 1 ms (85.81%), Space: 42.5 MB (76.35%) - LeetHub

--- 3870-count-commas-in-range/3870-count-commas-in-range.java
+++ 3870-count-commas-in-range/3870-count-commas-in-range.java
@@ -1,16 +1,4 @@
-// class Solution {
-//     public int countCommas(int n) {
-//         int len=0;
-//         while(n>0){
-//            int digit= n%10;
-//             len++;
-//            n=n/10;
-//         }
-//         if(len<4)
-//         return 0;
-//         return (len-1)/3;
-//     }
-// }
+
 class Solution {
     public int countCommas(int n) {
        if (n < 1000) {
