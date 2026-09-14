6b0238cdbc97c4e536c7a73f8a5d6739b81b9409
SHUBHAM <shubhamagrahari9191@gmail.com>
2026-09-15 00:32:04 +0530

    Time: 5 ms (29.18%), Space: 51.6 MB (82.6%) - LeetHub

---
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -0,0 +1,68 @@
+class Solution {
+    class Pair{
+        int row;
+        int col;
+        Pair(int row,int col){
+            this.row=row;
+            this.col=col;
+        }
+    }
+    public int numIslands(char[][] grid) {
+        int m=grid.length;
+        int n=grid[0].length;
+        int count=0;
+         boolean visited[][]=new boolean[m][n];
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(grid[i][j]=='1' && visited[i][j]==false){
+                    dfs(i,j,grid,visited);
+                    count++;
+                }
+            } 
+        }
+        return count;
+        
+    }
+    public void dfs(int i,int j,char grid[][],boolean visited[][]){
+         int m=grid.length;
+        int n=grid[0].length;
+        Queue<Pair> q=new LinkedList<>();
+        visited[i][j] = true;
+        q.add(new Pair(i,j));
+        while(q.size()>0){
+            Pair front=q.remove();
+            int row=front.row;
+            int col=front.col;
+            //top
+            if(row>0){
+                if(visited[row-1][col]==false && grid[row-1][col]=='1'){
+                    q.add(new Pair(row-1,col));
+                    visited[row-1][col]=true;
+                }
+            }
+            //bottom
+            if(row+1<m){
+                if(visited[row+1][col]==false && grid[row+1][col]=='1'){
+                    q.add(new Pair(row+1,col));
+                    visited[row+1][col]=true;
+                }
+            }
+            //left
+            if(col>0){
+                if(visited[row][col-1]==false && grid[row][col-1]=='1'){
+                    q.add(new Pair(row,col-1));
+                    visited[row][col-1]=true;
+                }
+            }
+            //right
+            if(col+1 <n){
+                if(visited[row][col+1]==false && grid[row][col+1]=='1'){
+                    q.add(new Pair(row,col+1));
+                    visited[row][col+1]=true;
+                }
+            }
+
+
+        }
+    }
+}
